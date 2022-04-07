ced4d6614843735148e356c6f8da4bc3007671f5
AbrilVelo <101335470+AbrilVelo@users.noreply.github.com>
2022-04-07 13:52:16 -0300

    Ejercicio1 y 2

---
+++ Assets/Scripts/Ejercicio1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio1 : MonoBehaviour
+{
+    //Crea un programa que permita ingresar por 
+    //Inspector dos valores enteros y responda si son iguales o no lo son.
+
+    public int num1;
+    public int num2;
+
+    void Start()
+    {
+
+
+
+        if (num1 > num2)
+        {
+            Debug.Log("El numero 1 es mayor");
+        }
+
+        if (num2 > num1)
+        {
+            Debug.Log("El numero 2 es mayor");
+        }
+        if (num1 == num2)
+        {
+          Debug.Log("Los numeros son iguales");
+        }
+        
+               
+       
+    }
+
+    
+}
---
+++ Assets/Scripts/Ejercicio2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2 : MonoBehaviour
+{
+
+    //Crear las variables necesarias para ingresar por 
+    //el precio de 3 productos y un monto de dinero disponible.
+    //si la suma de los mismos supera o no el monto de dinero disponible.
+    //Luego indicar cuánto dinero sobra o falta.
+
+    public float precio1;
+    public float precio2;
+    public float precio3;
+    public float montoDisponible;
+
+
+    void Start()
+    {
+        float conjunto = precio1 + precio2 + precio3;
+        float sobra;
+        float falta;
+
+        if (conjunto < montoDisponible)
+        {
+            sobra = montoDisponible - conjunto;
+            Debug.Log("Los precios de los productos no supera el monto de dinero disponible, sobran $" + sobra);
+        }
+        else if (conjunto > montoDisponible)
+        {
+            falta = conjunto - montoDisponible;
+            Debug.Log("Los precios de los productos supera el monto de dinero disponible, faltan $" + falta);
+        }
+        else if (conjunto == montoDisponible)
+        {
+            Debug.Log("Los precios de los productos no superan el monto de dinero disponible, faltan $0");
+        }
+
+    }
+
+    
+}
